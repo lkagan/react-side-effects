2745e3d5d728d2066086ee4a8c15b5e21a91e5f1
Larry Kagan <larry@superiocity.com>
2022-03-15 12:01:08 -0400

    Using Context with Consumer

--- src/App.js
+++ src/App.js
@@ -1,39 +1,44 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
+import AuthContext from "./store/auth-context";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  useEffect(() => {
-      if (localStorage.getItem('isLoggedIn') === '1') {
-          setIsLoggedIn(true);
-      }
-  }, [])
+    useEffect(() => {
+        if (localStorage.getItem('isLoggedIn') === '1') {
+            setIsLoggedIn(true);
+        }
+    }, [])
 
-  const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
-      localStorage.setItem('isLoggedIn', '1');
-    setIsLoggedIn(true);
-  };
+    const loginHandler = (email, password) => {
+        // We should of course check email and password
+        // But it's just a dummy/ demo anyways
+        localStorage.setItem('isLoggedIn', '1');
+        setIsLoggedIn(true);
+    };
 
-  const logoutHandler = () => {
-    setIsLoggedIn(false);
-    localStorage.setItem('isLoggedIn', '0');
-  };
+    const logoutHandler = () => {
+        setIsLoggedIn(false);
+        localStorage.setItem('isLoggedIn', '0');
+    };
 
-  return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
-      <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
-      </main>
-    </React.Fragment>
-  );
+    return (
+        <AuthContext.Provider
+            value={{
+                isLoggedIn: isLoggedIn,
+            }}
+        >
+            <MainHeader  onLogout={logoutHandler}/>
+            <main>
+                {!isLoggedIn && <Login onLogin={loginHandler}/>}
+                {isLoggedIn && <Home onLogout={logoutHandler}/>}
+            </main>
+        </AuthContext.Provider>
+    );
 }
 
 export default App;
