97dd0b2999d88202d2179abc013f991a687114f3
Larry Kagan <larry@superiocity.com>
2022-03-16 12:46:32 -0400

    Moved auth state management to context.  (💥 not working)

--- src/App.js
+++ src/App.js
@@ -1,44 +1,19 @@
-import React, {useState, useEffect} from 'react';
-
+import React, { useContext } from 'react';
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
 import AuthContext from "./store/auth-context";
 
 function App() {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-    useEffect(() => {
-        if (localStorage.getItem('isLoggedIn') === '1') {
-            setIsLoggedIn(true);
-        }
-    }, [])
-
-    const loginHandler = (email, password) => {
-        // We should of course check email and password
-        // But it's just a dummy/ demo anyways
-        localStorage.setItem('isLoggedIn', '1');
-        setIsLoggedIn(true);
-    };
-
-    const logoutHandler = () => {
-        setIsLoggedIn(false);
-        localStorage.setItem('isLoggedIn', '0');
-    };
-
+    const ctx = useContext(AuthContext)
     return (
-        <AuthContext.Provider
-            value={{
-                isLoggedIn: isLoggedIn,
-                onLogout: logoutHandler
-            }}
-        >
-            <MainHeader  onLogout={logoutHandler}/>
+        <React.Fragment>
+            <MainHeader  onLogout={ctx.logoutHandler}/>
             <main>
-                {!isLoggedIn && <Login onLogin={loginHandler}/>}
-                {isLoggedIn && <Home onLogout={logoutHandler}/>}
+                {!ctx.isLoggedIn && <Login/>}
+                {ctx.isLoggedIn && <Home/>}
             </main>
-        </AuthContext.Provider>
+        </React.Fragment>
     );
 }
 
